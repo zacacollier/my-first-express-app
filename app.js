b12fcb06c11b9e4967fa16839717fc1d9fcb5df9
zacacollier <zacacollier@gmail.com>
2016-10-13 19:13:32 -0500

    Lesson 1 Exercise Complete

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const path = require('path');
 
 const express = require('express');
 
+const ejs = require('ejs');
 
 const app = express();
 
+// app.get('/', function(req, res, next) {
+//   res.send(path.join(__dirname, 'views', 'index.ejs'));
+// });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// app.get('/', function(req, res, next) {
+//   res.render('index.ejs', { greeting: 'Hello from ejs!' });
+// });
+
 app.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+  res.render('index.ejs', { names: ['Rick', 'Morty', 'Summer', 'Beth', 'Jerry'] });
+});
+
 const port = 3000;
 const server = http.createServer(app);
 server.listen(port);
